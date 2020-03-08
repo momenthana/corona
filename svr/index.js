68f0b22b604d827fb75743baeafa0851763ff977
Hana <momenthana@kakao.com>
2020-03-08 22:02:14 +0900

    koa router 준비

--- svr/index.js
+++ svr/index.js
@@ -1,1 +1,15 @@
-console.log('svr')
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const koa = new Koa()
+const router = new Router()
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'root'
+})
+
+koa.use(router.routes()).use(router.allowedMethods())
+
+koa.listen(80, () => {
+  console.log('http://localhost:80')
+})
