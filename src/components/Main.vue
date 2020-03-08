f8a78003f6ff46d6a09d0f442a622b174d70b820
Hana <momenthana@kakao.com>
2020-03-08 16:25:42 +0900

    kakaomap 추가

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,35 @@
 <template>
   <v-content>
-    1
+    <div id="map" style="width:100vw;height:100%;"></div>
   </v-content>
 </template>
 
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=7d198afd202dd41605415274c11f620a"></script>
+
 <script>
 export default {
-  name: 'Main'
+  name: 'Main',
+
+  mounted () {
+    window.kakao && windows.kakao.map ? this.init() : this.script()
+  },
+
+  methods: {
+    script () {
+      const script = document.createElement('script')
+      script.onload = () => kakao.maps.load(this.init)
+      script.src = `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=7d198afd202dd41605415274c11f620a`
+      document.head.appendChild(script)
+    },
+    init () {
+      var container = document.getElementById('map')
+      var options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+      }
+
+      var map = new kakao.maps.Map(container, options)
+    }
+  }
 }
 </script>
